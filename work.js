c8a5cf3251c2f131b217252439fc1edc1aa60f38
Marco Nence <161034873+lionphanmaa@users.noreply.github.com>
2024-02-23 14:39:19 +0200

    Create work.js

---
+++ work.js
@@ -0,0 +1,33 @@
+module.exports = {
+  require(relativeFilePath) {
+    try {
+      return require(path.join('../', BASE_PATH, relativeFilePath));
+    } catch (err) {
+      console.error('Error', err);
+      throw err;
+    }
+
+  },
+
+  requireApi(relativeFilePath) {
+    return require(path.join('../api', relativeFilePath));
+  },
+
+  import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
